25eadf31d82e19dd2c6c20a8b7b765c77aaa84cd
Hugo Puhlmann <hugopuhlmann@gmail.com>
2015-04-06 00:22:49 -0300

    Switching from C++11 core to C++14.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -48,7 +48,7 @@ flags = [
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=c++11',
+'-std=c++14',
 '-stdlib=libc++',
 pkg_config('glfw3'),
 '-Iexternal/gl3w/include'
