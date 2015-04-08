3009dea76a3771520502cb8656a7cd46f50bb92c
Hugo Puhlmann <hugopuhlmann@gmail.com>
2015-04-08 02:59:18 -0300

    More normalization.

--- src/mat.hpp
+++ src/mat.hpp
@@ -103,73 +103,74 @@ decltype(auto) mat<T, M, N>::data() { return _data.data(); }
 
 template<class T, unsigned M, unsigned N>
 mat<T, M, N> operator+(const mat<T, M, N>& lhs, const mat<T, M, N>& rhs) {
-    mat<T, N, M> r;
-    for (unsigned i = 0; i < N * M; ++i) {
-	r[i] = lhs[i] + rhs[i];
-    }
-    return r;
+	mat<T, N, M> r;
+	for (unsigned i = 0; i < N * M; ++i) {
+		r[i] = lhs[i] + rhs[i];
+	}
+	return r;
 }
 
 template<class T, unsigned M, unsigned N>
 mat<T, N, M> operator-(const mat<T, N, M>& lhs, const mat<T, N, M>& rhs) {
-    mat<T, N, M> r;
-    for (auto i = 0U; i < N * M; ++i) {
-	r[i] = lhs[i] - rhs[i];
-    }
-    return r;
+	mat<T, N, M> r;
+	for (auto i = 0U; i < N * M; ++i) {
+		r[i] = lhs[i] - rhs[i];
+	}
+	return r;
 }
 
 template<class T, unsigned M, unsigned N>
 mat<T, N, M> operator*(const mat<T, N, M>& m, float scalar) {
-    mat<T, N, M> r;
-    for (auto i = 0U; i < N * M; ++i) {
-	r[i] = m[i] * scalar;
-    }
-    return r;
+	mat<T, N, M> r;
+	for (auto i = 0U; i < N * M; ++i) {
+		r[i] = m[i] * scalar;
+	}
+	return r;
 }
 
 template<class T, unsigned M, unsigned N>
 mat<T, N, M> operator*(float scalar, const mat<T, N, M>& m) {
-    return m * scalar;
+	return m * scalar;
 }
 
 template<class T, unsigned M, unsigned N, unsigned P>
 mat<T, N, P> operator*(const mat<T, M, N>& lhs, const mat<T, N, P>& rhs) {
-    mat<T, N, P> r;
-    for (auto i = 0U; i < M; ++i) {
-	for (auto j = 0U; j < P; ++j) {
-	    r(i, j) = 0;
-	    for (auto k = 0; k < N; ++k) {
-		r(i, j) += lhs(i, k) * rhs(k, j);
-	    }
+	mat<T, N, P> r;
+	for (auto i = 0U; i < M; ++i) {
+		for (auto j = 0U; j < P; ++j) {
+			r(i, j) = 0;
+			for (auto k = 0; k < N; ++k) {
+				r(i, j) += lhs(i, k) * rhs(k, j);
+			}
+		}
 	}
-    }
-    return r;
+	return r;
 }
 
 template<class T, unsigned M, unsigned N>
 mat<T, N, M> transpose(const mat<T, M, N>& m) {
-    mat<T, N, M> r;
-
-    for (auto i = 0U; i < N; ++i) {
-	for (auto j = 0U; j < M; ++j) {
-	    r(i, j) = m(j, i);
+	mat<T, N, M> r;
+	for (auto i = 0U; i < N; ++i) {
+		for (auto j = 0U; j < M; ++j) {
+			r(i, j) = m(j, i);
+		}
 	}
-    }
+	return r;
+}
 
     return r;
 }
 
 template<class T, unsigned M, unsigned N>
 std::ostream& operator<<(std::ostream& stream, const mat<T, M, N>& mat) {
-    stream << "mat<" << M << ',' <<  N << ">: " << std::endl;
-    for (auto i = 0U; i < M; ++i) {
-	for (auto j = 0U; j < N; ++j) {
-	    stream << mat(i, j) << ' ';
+	stream << "mat<" << M << ',' <<  N << ">: " << std::endl;
+	for (auto i = 0U; i < M; ++i) {
+		for (auto j = 0U; j < N; ++j) {
+			stream << mat(i, j) << ' ';
+		}
+		stream << std::endl;
 	}
-	stream << std::endl;
-    }
-    return stream;
+	return stream;
 }
 
-}
+} //namespace math
